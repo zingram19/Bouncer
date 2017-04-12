d7dce17abfdc2c5176eed853eaa1e3b3f3e8a12a
zingram19 <zingram19@punahou.edu>
2017-04-11 20:02:59 -1000

    Added 8 px to edge of platform to make game less glitchy

--- level.cpp
+++ level.cpp
@@ -171,7 +171,7 @@ void level::detectCollision(d_t* d) {
     //BL
     else if (col & 4) {
         // collided on bottom of dog
-        if (TILE_DIM - ((int) d->x % TILE_DIM) > (int) d->y % 64) {
+        if ((TILE_DIM - ((int) d->x % TILE_DIM)) + 8 > (int) d->y % 64) {
             d->y = (1 + (int) d->y / TILE_DIM) * TILE_DIM - DOG_W;
             d->yv = 0;
             d->onGround = true;
